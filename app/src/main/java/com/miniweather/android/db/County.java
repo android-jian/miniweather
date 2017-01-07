f8f6de95aa179965cbcbc3a072ad1c39e4d00d7e
android-jian <2424055636@qq.com>
2017-01-07 14:12:11 +0800

    完成数据表的操作

---
+++ app/src/main/java/com/miniweather/android/db/County.java
@@ -0,0 +1,50 @@
+package com.miniweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dell on 2017/1/7.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+}
---
+++ app/src/main/java/com/miniweather/android/db/Province.java
@@ -0,0 +1,40 @@
+package com.miniweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dell on 2017/1/7.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
