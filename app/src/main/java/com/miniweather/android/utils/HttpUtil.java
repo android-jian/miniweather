6b845c8b010a1dcaaac7faa2c57941092c8915e5
android-jian <2424055636@qq.com>
2017-01-07 16:46:18 +0800

    完成省市县三级列表功能

---
+++ app/src/main/java/com/miniweather/android/utils/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.miniweather.android.utils;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by dell on 2017/1/7.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
